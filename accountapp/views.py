d3425871687a047c1a09f5f0ccff0c74abc24e58
Jangyj99 <jangyunji0723@naver.com>
2022-06-09 15:38:02 +0900

    Use createview

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -20,3 +23,13 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+# 장고에서 기본으로 제공해주는 모델인 User 사용
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    #success_url이란 이 계정을 만들기 성공하면 어느 경로로 재 연결
+    template_name = 'accountapp/create.html'
+    #회원 가입을 할 떄 볼 html. form. 어떤 html파일?
